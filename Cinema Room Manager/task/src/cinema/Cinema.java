1e08d1da00e340a4daef93f14c96f9a58749441b
Edwin Silva <edwinsils@hotmail.com>
2022-11-29 17:41:40 -0500

    Se realizan cambios sobre el proyecto 2- Cinema Room Manager

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -4,8 +4,32 @@ public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
-        Scanner scanner = new Scanner(System.in);
 
+        String[][] matrix = roomConstruction();
+        int option  = optionMenu();
+        while (option != 0) {
+            if (option == 1)  printMatrix(matrix);
+            else if (option == 2) buyTicket(matrix);
+            option  = optionMenu();
+        }
+
+    }//main
+
+    public static int TicketValue(int row, int rows, int columns) {
+        int value = 0;
+        if (rows * columns <= 60) {
+            value = 10;
+        } else if (row <= rows / 2) {
+            value = 10;
+        } else {
+            value = 8;
+        }
+        return value;
+    }
+
+    public static String[][] roomConstruction() {
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the number of rows: ");
         System.out.print("> ");
@@ -15,24 +39,55 @@ public class Cinema {
         System.out.print("> ");
         int columns = scanner.nextInt();
 
+        String[][] matrix = new String[rows + 1][columns + 1];
+
         //print the matrix
         System.out.println();
         System.out.println("Cinema:");
-        for (int i = 1 ; i <= columns; i++) {
-            if (i == 1) {
-                System.out.print("\s\s1 ");
-            } else {
-                System.out.print(i + " ");
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++){
+                if (i == 0 && j == 0) {
+                    matrix[i][j] = " ";
+                } else if (i == 0) {
+                    matrix[i][j] = String.valueOf(j);
+                } else if (j == 0) {
+                    matrix[i][j] = String.valueOf(i);
+                } else {
+                    matrix[i][j] = "S";
+                }
             }
         }
+        return matrix;
+    }
+
+    public static void printMatrix(String[][] matrix) {
         System.out.println();
-        //body
-        for (int i = 1; i <= rows; i++) {
-            System.out.print(i + " ");
-            for (int j = 1; j <= columns; j++)
-                System.out.print("S ");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
             System.out.println();
         }
+        System.out.println();
+    }
+
+    public static int optionMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. Show the seats");
+        System.out.println("2. Buy a tickets");
+        System.out.println("0. Exit");
+        System.out.print("> ");
+        int option = scanner.nextInt();
+        return option;
+    }
+
+
+    public static void buyTicket(String[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        Scanner scanner = new Scanner(System.in);
 
         //values of the tickets
         System.out.println();
@@ -55,52 +110,10 @@ public class Cinema {
             System.out.print("> ");
             seat = scanner.nextInt();
         }
+        System.out.println("Ticket price: $" + TicketValue(row, rows, columns));
         System.out.println();
-        System.out.println("Ticket price: $" + valorTicket(row, rows, columns));
 
-        //print cordinates of seat
 
-        String[][] matrix = new String[rows + 1][columns + 1];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (i == 0) {
-                    if (j == 0) {
-                        matrix[i][j] = "  ";
-                    } else {
-                        matrix[i][j] = String.valueOf(j) + " ";
-                    }
-                } else if (j == 0) {
-                    matrix[i][j] = String.valueOf(i) + " ";
-                } else {
-                    matrix[i][j] = "S ";
-                }
-            }
-        }
-        //print matrix
-        System.out.println();
-        System.out.println("Cinema:");
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (i == row && j == seat) {
-                    System.out.print("B ");
-                }else {
-                    System.out.print(matrix[i][j]);
-                }
-            }
-            System.out.println();
-        }
-
-    }//main
-
-    public static int valorTicket(int row, int rows, int columns) {
-        int value = 0;
-        if (rows * columns <= 60) {
-            value = 10;
-        } else if (row <= rows / 2) {
-            value = 10;
-        } else {
-            value = 8;
-        }
-        return value;
     }
+
 }//class
